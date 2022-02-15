286c70882f0fcdbfa676a79e7b621fb38547edc7
Rohit967367 <rohitpramanik30593@gmail.com>
2022-02-15 14:42:25 +0530

    fifth commit

--- server.js
+++ server.js
@@ -18,22 +18,22 @@ app.get("/", (req, res) => {
   res.send("hello rohit");
 });
 
-app.get("/hakuna-matata", (req, res) => {
-  const API_KEY = "2e817630-b783-455a-acb9-b836fe712bba";
-  const SECRET_KEY =
-    "0a2d3a665153637278fcd10c8f9ad51e8660e65d747dd20b88a7fcb3dbe49d43";
+// app.get("/hakuna-matata", (req, res) => {
+//   const API_KEY = "2e817630-b783-455a-acb9-b836fe712bba";
+//   const SECRET_KEY =
+//     "0a2d3a665153637278fcd10c8f9ad51e8660e65d747dd20b88a7fcb3dbe49d43";
 
-  const options = { expiresIn: "10m", algorithm: "HS256" };
+//   const options = { expiresIn: "10m", algorithm: "HS256" };
 
-  const payload = {
-    apikey: API_KEY,
-    Permissions: ["allow_join", "allow_mod"],
-  };
+//   const payload = {
+//     apikey: API_KEY,
+//     Permissions: ["allow_join", "allow_mod"],
+//   };
 
-  const token = jwt.sign(payload, SECRET_KEY, options);
+//   const token = jwt.sign(payload, SECRET_KEY, options);
 
-  res.json({ token });
-});
+//   res.json({ token });
+// });
 
 app.listen(PORT, () => {
   console.log(`API server listening at http://locahost:${PORT}`);
